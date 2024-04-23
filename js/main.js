8bc0b74a5657d690c9f59b4b6dfd91e22b9e00e0
SnowDogge <128687888+SnowDogge@users.noreply.github.com>
2024-04-23 10:42:47 +0900

    [20240423] new repo

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    var window_w
+
+    function device_chk(){
+        window_w = $(window).scrollTop();
+        console.log(window_w);
+    }
+    
+    device_chk();
+
+    $(window).scroll(function(){
+        device_chk();
+        if (window_w >= 1400) {
+            $('.gnb ul li').removeClass('on');
+            $('.gnb ul .gnb3').addClass('on');
+        }
+        if (window_w >= 450 && window_w < 1400) {
+            $('.gnb ul li').removeClass('on');
+            $('.gnb ul .gnb2').addClass('on');
+        }
+        if (window_w < 450) {
+            $('.gnb ul li').removeClass('on');
+            $('.gnb ul .gnb1').addClass('on');
+        }
+        if (window_w < 200) {
+            $('.skill .progress').removeClass('on');
+        }
+        if (window_w > 1500) {
+            $('.skill .progress').removeClass('on');
+        }
+        if (window_w > 600 && window_w < 1100) {
+            $('.skill .progress').addClass('on');
+        }
+    })
+    $('.gnb .gnb1').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+    })
+    $('.gnb .gnb2').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 850
+        }, 500)
+    })
+    $('.gnb .gnb3').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 1770
+        }, 500)
+    })
+})
